43201a3e6c2650d936243220a465a1c3697aa79b
sam kelly <samkelly1290@gmail.com>
2022-03-03 11:37:23 +1100

    i think this is done

--- test.sh
+++ test.sh
@@ -7,5 +7,6 @@ for test in tests/*.in
 do
     test_name=$(echo $test | cut -d . -f1)
     echo Running Test: $test_name
+    # $test_name.out <(./vr2017 $(cat $test))
     diff $test_name.out <(./vr2017 $(cat $test)) && echo "Test $test_name passed." || echo "Test $test_name failed."
 done
