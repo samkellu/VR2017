026e7f992b0d85c1b06adc081fb0cbf3d4ef6938
sam kelly <samkelly1290@gmail.com>
2022-03-10 02:41:08 +1100

    submit

--- vr2017.c
+++ vr2017.c
@@ -216,7 +216,7 @@ int main(int argc, char **argv) {
             //Checks the validity of the current packet against the values in the most recent valid packet.
             //If it is invalid, the packet is skipped, otherwise it is set to be the new most recent valid
             //packet
-            printf("%d, %d", last_valid_packet[coord], swizzled_packet[coord]);
+            printf("%d, %d\n", last_valid_packet[coord], swizzled_packet[coord]);
             if (abs(swizzled_packet[coord] - last_valid_packet[coord]) > 25) {
               printf("        Ignoring packet. %c: %d. Previous valid packet's %c: %d. %d > 25.\n", coords[coord], swizzled_packet[coord], coords[coord], last_valid_packet[coord], abs(last_valid_packet[coord] - swizzled_packet[coord]));
               break;
@@ -231,11 +231,13 @@ int main(int argc, char **argv) {
               last_valid_packet[coord] = swizzled_packet[coord];
               sums[coord] += last_valid_packet[coord];
             }
-          } else {
-            printf("    No valid packets were found for this chunk.\n\n");
           }
         }
-        printf("    Chunk Average X: %.2f, Average Y: %.2f, Average Z: %.2f\n\n", (double)sums[0]/(double)valid_count,(double)sums[1]/(double)valid_count, (double)sums[2]/(double)valid_count);
+        if (valid_count == 0) {
+          printf("    No valid packets were found for this chunk.\n\n");
+        } else {
+          printf("    Chunk Average X: %.2f, Average Y: %.2f, Average Z: %.2f\n\n", (double)sums[0]/(double)valid_count,(double)sums[1]/(double)valid_count, (double)sums[2]/(double)valid_count);
+        }
         //resets chunk parameters at the end of the chunk
         offset_current_chunk = 0;
         valid_count = 0;
