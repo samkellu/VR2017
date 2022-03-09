c57281abac47d07859905f9349bf3430d4109158
sam kelly <samkelly1290@gmail.com>
2022-03-10 02:06:07 +1100

    testcases

--- vr2017.c
+++ vr2017.c
@@ -104,6 +104,15 @@ int main(int argc, char **argv) {
     }
     //Reads a value into the current chunk's array
     unprocessed_chunk[offset_current_chunk] = fgetc(file);
+    //Check for the case in which the chunk is larger than the specified limit
+    printf("%d\n", offset_current_chunk);
+    if (offset_current_chunk == 639) {
+      printf("Chunk: %d at offset: %d\n", chunk_count, offset - offset_current_chunk);
+      chunk_count++;
+      printf("Error: Chunk size exceeds the maximum allowable chunk size of 640 bytes.\n\n");
+      offset_current_chunk = 0;
+      continue;
+    }
     //Checks if the value read in is the EOF character and records it as such
     if (unprocessed_chunk[offset_current_chunk] == EOF) {
       unprocessed_chunk[offset_current_chunk] = '\0';
@@ -129,13 +138,6 @@ int main(int argc, char **argv) {
         //processes it if it is.
         if ((offset_current_chunk - 4) % 5 != 0 && !(feof(file) && offset_current_chunk % 5 != 5)) {
           printf("Error: Chunk must be divisible by 5 bytes.\n");
-          offset_current_chunk = 0;
-          continue;
-        }
-        //Check for the case in which the chunk is larger than the specified limit
-        printf("%d\n", offset_current_chunk);
-        if (offset_current_chunk == 639) {
-          printf("Error: Chunk size exceeds the maximum allowable chunk size of 640 bytes.\n\n");
           offset_current_chunk = 0;
           continue;
         }
